34ba5cb2b7a029ea1183f467e207ae4591eabc1f
Joel <joelbmolina@gmail.com>
2016-12-08 17:56:05 -0700

    established basic express server

--- server/server.js
+++ server/server.js
@@ -6,6 +6,6 @@ const app = express();
 app.listen(8000);
 
 app.use(morgan('dev'));
-app.use(express.static(__dirname + '/../client'));
+app.use(express.static(__dirname + '/../client/compile/'));
 
 module.exports = app;
