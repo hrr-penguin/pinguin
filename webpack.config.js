be31c1901b7c97425382e8413b86c93204f66b94
Johanna Tchon <jktchon@gmail.com>
2016-12-08 23:51:08 -0800

    set up react with webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  entry: ['./src/index.js'],
+  output: {
+    path: './public',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'css-loader',
+        query: {
+          modules: true,
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.json', '.jsx']
+  }
+};
