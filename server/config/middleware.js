0f3086ddee4866c39661d26ee117af03891cd92d
Joel <joelbmolina@gmail.com>
2016-12-09 17:54:14 -0700

    started authentication process

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,6 +1,21 @@
 const morgan = require('morgan');
+const bodyParse = require('body-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 module.exports = function(app, express) {
+
   app.use(morgan('dev'));
+
+  app.use(require('express-session')({
+    secret: 'secrets',
+    resave: false,
+    saveUnitiliazed: false
+  }));
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
   app.use(express.static(__dirname + '/../client/public/'));
+
 };
--- server/dbConfig/config.js
+++ server/dbConfig/config.js
@@ -1,8 +1,1 @@
-const path = require('path');
-const knex = require('knex')({
-  client: 'sqlLite3',
-  connection: {
-    filename: path.join(__dirname + '/../../database/pinguin.sqlite')
-  },
-  useNullasDefault: true
-});
+
