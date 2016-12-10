40ef5db3050d446f819bd6f3e4de885b07303b11
Doug Vaughn <douglas.s.vaughn@gmail.com>
2016-12-10 07:38:16 -0800

    Update routes.js
    Middleware belongs in the middleware.js file.

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,9 +1,4 @@
 module.exports = function(app, express) {
-  const passport = require('passport');
-  const bodyParser = require('body-parser');
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(bodyParser.json());
-
   app.get('/api/feed', (req,res) => {console.log("success server get req.param", req.param)});
   app.post('/api/signup', (req,res) => {console.log("success server post req.body", req.body)});
   app.post('/api/signin', passport.authenticate('local', {
