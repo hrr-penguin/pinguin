56a15e1a76713302e18faf8c331c1d75cb2d3eb9
Doug Vaughn <douglas.s.vaughn@gmail.com>
2016-12-10 15:26:05 -0800

    Query the database for the list of feeds for the user.

--- server/helpers/helpers.js
+++ server/helpers/helpers.js
@@ -1,6 +1,15 @@
+const Models = require('../dbConfig/models/models.js');
+
 module.exports = {
   getFeed: function(req, res) {
-    console.log("success server get req.param", req.param);
+    // TODO: send in user id instead of the hard coded 1.
+    Models.feeds.get(1, function(err, results) {
+      if (err) {
+        console.log('helpers.getFeed error: ', error);
+      } else {
+        res.json({ urls: results.map(item => item.uri) });
+      }
+    })
   },
 
   signUp: function(req, res) {
