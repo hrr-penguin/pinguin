f2fe9c772c45f7db529220c9983b83f63de53740
Joel <joelbmolina@gmail.com>
2016-12-09 21:11:08 -0700

    added signup to model

--- server/dbConfig/models/models.js
+++ server/dbConfig/models/models.js
@@ -1,4 +1,6 @@
 const db = require('../../db/db.js');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 module.exports = {
   users: {
@@ -6,6 +8,14 @@ module.exports = {
       let query = 'SELECT * FROM users WHERE user = ?';
       db.query(query, function(err, results) {
         callback(err, results);
+      });
+    },
+    post: function(user, password, callback) {
+      let query = 'INSERT INTO users(username, password) value (?,?)';
+      bcrypt.hash(password, saltRounds, function(err, hash) {
+        db.query(query, [user, hash], function(err, results) {
+          callback(err, results);
+        });
       });
     }
   }
