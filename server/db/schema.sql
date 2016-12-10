5f8a163114ee3ea9fc22e8c9f42e45adbd37405b
Doug Vaughn <douglas.s.vaughn@gmail.com>
2016-12-09 16:44:09 -0800

    Added MySQL database.

---
+++ server/db/schema.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE rss;
+
+USE rss;
+
+CREATE TABLE users (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  username varchar(20) NOT NULL UNIQUE,
+  password varchar(255) NOT NULL,
+  subscription int
+);
+
+CREATE TABLE feeds (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  uri varchar(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE users_feeds (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  user_id int NOT NULL,
+  feed_id int NOT NULL,
+  FOREIGN KEY(user_id) REFERENCES users(id),
+  FOREIGN KEY(feed_id) REFERENCES feeds(id)
+);
