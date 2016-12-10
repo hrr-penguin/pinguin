580ff600af21f5e25546148d2418b0ef60a3339c
Doug Vaughn <douglas.s.vaughn@gmail.com>
2016-12-10 10:19:08 -0800

    Fixed dependencies.

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,3 +1,5 @@
+const passport = require('passport');
+
 module.exports = function(app, express) {
   app.get('/api/feed', (req,res) => {console.log("success server get req.param", req.param)});
   app.post('/api/signup', (req,res) => {console.log("success server post req.body", req.body)});
